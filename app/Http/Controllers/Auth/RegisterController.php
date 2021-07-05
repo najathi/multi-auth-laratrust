059a57341246c566579922320c1d392ca63e2534
najathi <najathi777@gmail.com>
2021-07-05 12:39:54 +0530

    tweak

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,6 +71,7 @@ class RegisterController extends Controller
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        // $user->attachRole('user');
         $user->attachRole($data['role_id']);
         return $user;
 
